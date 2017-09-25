417e6dd1c45e564809f75e177613343537ce1ad1
barracoota <owl1n@mail.ru>
2017-09-25 12:16:46 +0500

    initial

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Translator from './packages/translations'
+
+const install = (Vue, transObj = {}) => {
+    if (install.installed) return;
+    install.installed = true;
+
+    Vue.prototype.$trans = Translator.trans;
+    Vue.prototype.$transObject = transObj;
+
+    if (typeof window !== 'undefined' && window.Vue) {
+        install(window.Vue);
+    }
+};
