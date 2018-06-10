302f217e8c923c1d3c7aff04f9a6fcd72df09660
Maxim Markin <owl1n@mail.ru>
2018-06-10 12:12:57 +0500

    translations

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 let component = null;
+const each = require('lodash/each');
+const get = require('lodash/get');
+
+const buildValue = (value, params) => {
+  let result = value;
+  if (Object.keys(params).length) {
+    each(params, (value, param) => {
+      result = result.replace(':' + param, value)
+    });
+  }
+  
+  return result;
+};
 
 const Translator = {
   install(Vue) {
@@ -6,7 +19,7 @@ const Translator = {
       component = new Vue({
         data() {
           return {
-            language: '',
+            language: 'en', // default language
             translations: {},
           }
         },
@@ -17,6 +30,68 @@ const Translator = {
 
           load(object) {
             this.translations = object;
+          },
+          
+          setup(language, object) {
+            this.setLang(language);
+            this.load(object);
+          },
+          
+          translate(path, params = {}) {
+            let arrayedPath = path.split('.'); // 0 - language, 1 - key;
+            let group = null;
+      
+            if (!get(this.translations, arrayedPath[0])) {
+              group = arrayedPath[0];
+              delete arrayedPath[0];
+            } else {
+              delete arrayedPath[0]; delete arrayedPath[1];
+            }
+            
+            if (!get(this.translations, `${this.language}.${group}`)) {
+              console.error('translator - (' + path + ') group not found.');
+              return path;
+            }
+  
+            arrayedPath = arrayedPath.filter((o) => o);
+            let item = arrayedPath.join('.');
+            
+            if (!get(this.translations, `${this.language}.${group}.${item}`)) {
+              let findedKeys = {};
+              each(this.translations[this.language][group], (value, key) => {
+                if (key.indexOf(item + '.') !== -1) {
+                  let newKey = key.replace(item + '.', '');
+                  findedKeys[newKey] = buildValue(value, params);
+                }
+              });
+  
+              if (Object.keys(findedKeys).length) {
+                let initData = null;
+                each(findedKeys, (value, key) => {
+                  if (key.indexOf('.') !== -1) {
+                    let keys = key.split('.');
+        
+                    initData = findedKeys;
+                    each(keys, (k, i) => {
+                      if (initData[k] === undefined)
+                        initData[k] = {};
+          
+                      if (i === keys.length - 1) initData[k] = value;
+                      else initData = initData[k];
+                    });
+        
+                    delete findedKeys[key];
+                  }
+                });
+    
+                return findedKeys;
+              }
+  
+              console.error('translator - (' + path + ') item not found.');
+              return path;
+            }
+            
+            return buildValue(this.translations[this.language][group][item], params);
           }
         }
       });
