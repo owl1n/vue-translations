9832bad73035927c73f0bdf831de40779d6c0041
mean-cj <register@ireallyhost.com>
2019-07-19 01:01:47 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,112 +1,135 @@
-let component = null;
-const each = require('lodash/each');
-const get = require('lodash/get');
+/**
+ * VueTranslate plugin v0.1.4
+Maxim Markin <owl1n@mail.ru>**/
+"use strict";
 
-const buildValue = (value, params) => {
-  let result = value;
+var component = null;
+var each = require("lodash/each");
+var get = require("lodash/get");
+var VueRouter = require("vue-router");
+
+var buildValue = function(value, params) {
+  var result = value;
   if (Object.keys(params).length) {
-    each(params, (value, param) => {
-      result = result.replace(':' + param, value)
+    each(params, function(value, param) {
+      result = result.replace(":" + param, value);
     });
   }
-  
+
   return result;
 };
 
-const Translator = {
-  install(Vue) {
+var Translator = {
+  install: function(Vue, options) {
     if (!component) {
       component = new Vue({
-        data() {
+        data: function() {
           return {
-            language: 'en', // default language
-            translations: {},
-          }
+            language: "en", // default language
+            translations: {}
+          };
         },
+
         methods: {
-          setLang(lang) {
+          setLang: function(lang) {
             this.language = lang;
           },
-
-          load(object) {
+          load: function(object) {
             this.translations = object;
           },
-          
-          setup(language, object) {
+          setup: function(language, object) {
             this.setLang(language);
             this.load(object);
           },
-          
-          translate(path, params = {}) {
-            let arrayedPath = path.split('.'); // 0 - language, 1 - key;
-            let group = null;
-      
+          translate: function(path) {
+            // console.log(options.$router.currentRoute);
+            //console.info("App this router:", window.location.pathname);
+            var params =
+              arguments.length > 1 && arguments[1] !== undefined
+                ? arguments[1]
+                : {};
+
+            var arrayedPath = path.split("."); // 0 - language, 1 - key;
+            var group = null;
+
             if (!get(this.translations, arrayedPath[0])) {
               group = arrayedPath[0];
               delete arrayedPath[0];
             } else {
-              delete arrayedPath[0]; delete arrayedPath[1];
+              delete arrayedPath[0];
+              delete arrayedPath[1];
             }
-            
-            if (!get(this.translations, `${this.language}.${group}`)) {
-              console.error('translator - (' + path + ') group not found.');
-              return path;
+
+            if (!get(this.translations, this.language + "." + group)) {
+              // console.error("translator - (" + path + ") group not found.");
+              return path.split(".")[1];
             }
-  
-            arrayedPath = arrayedPath.filter((o) => o);
-            let item = arrayedPath.join('.');
-            
-            if (!get(this.translations, `${this.language}.${group}.${item}`)) {
-              let findedKeys = {};
-              each(this.translations[this.language][group], (value, key) => {
-                if (key.indexOf(item + '.') !== -1) {
-                  let newKey = key.replace(item + '.', '');
+
+            arrayedPath = arrayedPath.filter(function(o) {
+              return o;
+            });
+            var item = arrayedPath.join(".");
+
+            if (
+              !get(this.translations, this.language + "." + group + "." + item)
+            ) {
+              var findedKeys = {};
+              each(this.translations[this.language][group], function(
+                value,
+                key
+              ) {
+                if (key.indexOf(item + ".") !== -1) {
+                  var newKey = key.replace(item + ".", "");
                   findedKeys[newKey] = buildValue(value, params);
                 }
               });
-  
+
               if (Object.keys(findedKeys).length) {
-                let initData = null;
-                each(findedKeys, (value, key) => {
-                  if (key.indexOf('.') !== -1) {
-                    let keys = key.split('.');
-        
+                var initData = null;
+                each(findedKeys, function(value, key) {
+                  if (key.indexOf(".") !== -1) {
+                    var keys = key.split(".");
+
                     initData = findedKeys;
-                    each(keys, (k, i) => {
-                      if (initData[k] === undefined)
-                        initData[k] = {};
-          
+                    each(keys, function(k, i) {
+                      if (initData[k] === undefined) initData[k] = {};
+
                       if (i === keys.length - 1) initData[k] = value;
                       else initData = initData[k];
                     });
-        
+
                     delete findedKeys[key];
                   }
                 });
-    
+
                 return findedKeys;
               }
-  
-              console.error('translator - (' + path + ') item not found.');
+
+              console.error("translator - (" + path + ") item not found.");
               return path;
             }
-            
-            return buildValue(this.translations[this.language][group][item], params);
+
+            return buildValue(
+              this.translations[this.language][group][item],
+              params
+            );
           }
         }
       });
-      
+      console.log(component);
       Vue.prototype.$translate = component;
       // simple use
       Vue.prototype.__t = component.translate;
     }
-  },
+  }
 };
 
-if (typeof exports === 'object') {
+if (typeof exports === "object") {
   module.exports = Translator; // CommonJS
-} else if (typeof define === 'function' && define.amd) {
-  define([], function () { return Translator; }); // AMD
+} else if (typeof define === "function" && define.amd) {
+  define([], function() {
+    return Translator;
+  }); // AMD
 } else if (window.Vue) {
   window.Translator = Translator; // Browser (not required options)
   Vue.use(Translator);
